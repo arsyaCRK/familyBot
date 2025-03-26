2603cd54fcd445adfe9067b73bca21710323d10f
Arslan Bazrbayev <spb@mmsmart.ru>
2025-03-26 12:52:14 +0300

    1. Добавлен новый функционал.

--- app.go
+++ app.go
@@ -18,10 +18,12 @@ const (
 	BallyState     = "Проверить баллы 🚩"
 	Help           = "Справка 🆘"
 	BackToMainMenu = "Вернуться в главное меню ⬅️"
+	Back           = "Вернуться ⬅️"
 )
 
 var (
 	token = os.Getenv("TOKEN")
+	kbd   string
 )
 
 func main() {
@@ -51,6 +53,7 @@ func main() {
 		if update.Message.IsCommand() {
 			switch update.Message.Command() {
 			case "start":
+				sendMessage(bot, update.Message.Chat.ID, "Привет! Я ваш новый семйный Telegram-бот для удобвтсва работы с заданиями и начилсения баллов. 😉")
 				sendStartMenu(bot, update.Message.Chat.ID)
 			default:
 				continue
@@ -60,25 +63,38 @@ func main() {
 			case Help:
 				sendMessage(bot, update.Message.Chat.ID, "Хорошие дела и баллы!\n\n1. Помощь по дому 🏠\nУборка в своей комнате — 5 баллов\nМытьё посуды — 5 баллов\nВынос мусора — 3 балла\nПомощь в готовке еды — 7 баллов\nПомощь в уборке по дому - 7 баллов. \n\n2. Учёба и саморазвитие 👨‍🎓\nВыполненные домашние задания без напоминаний — 10 баллов\nЧтение книги (каждые 30 минут) — 7 баллов\nВыученное новое стихотворение или песня — 10 баллов\nУлучшение оценок по предмету — 15 баллов\n\n3. Доброта и хорошее поведение 👍\nПомощь брату или сестре — 7 баллов\nВежливость и отсутствие конфликтов весь день — 5 баллов\nДобрые поступки (например, помочь соседу или другу) — 10 баллов\n\n4. Физическая активность 🔋\nУтренняя зарядка или упражнения — 5 баллов\nПрогулка на свежем воздухе (более 1 часа) — 5 баллов\nУчастие в спортивных занятиях или соревнованиях — 10 баллов\n\n---\n\nНаграды за баллы! 🏆\n\n1. Маленькие награды (до 30 баллов):\nДополнительное время на планшет (30 минут) — 20 баллов\nНаклейка или маленькая игрушка — 25 баллов\nСладость (например, шоколадка) — 30 баллов\n\n2. Средние награды (30–70 баллов):\nПоход в кино или кафе — 50 баллов\nВыбор мультфильма или фильма для вечера — 50 баллов\nЛего или настольная игра — 70 баллов\n\n3. Крупные награды (70+ баллов):\nБольшая игрушка — 100 баллов\nПоездка в парк аттракционов — 150 баллов\nДень без домашних обязанностей — 70 баллов.")
 			case BallyPlus:
-				if update.Message.From.UserName == "Hbmkhg" || update.Message.From.FirstName == "Богданыч" {
+				switch update.Message.From.UserName {
+				case "arsyacrk", "Alyona_270688":
+					kbd = "plus"
+					sendPlusBally(bot, update.Message.Chat.ID)
+				default:
 					sendMessage(bot, update.Message.Chat.ID, "Вам зарпещено начислять баллы!")
 					continue
-				} else {
-					sendPlusBally(bot, update.Message.Chat.ID)
 				}
 			case BallyMinus:
-				{
-					if update.Message.From.UserName == "Hbmkhg" || update.Message.From.FirstName == "Богданыч" {
-						sendMessage(bot, update.Message.Chat.ID, "Вам зарпещено начислять баллы!")
-						continue
-					} else {
-						sendMinusBally(bot, update.Message.Chat.ID)
-					}
+				switch update.Message.From.UserName {
+				case "arsyacrk", "Alyona_270688":
+					kbd = "minus"
+					sendMinusBally(bot, update.Message.Chat.ID)
+				default:
+					sendMessage(bot, update.Message.Chat.ID, "Вам зарпещено списывать баллы!")
+					continue
 				}
 			case BackToMainMenu:
 				sendStartMenu(bot, update.Message.Chat.ID)
 			case BallyState:
 				loadScores(bot, update.Message.Chat.ID)
+			case Bogdan, Veronika:
+				kbdSectionsPlus(bot, update.Message.Chat.ID)
+			case Back:
+				switch kbd {
+				case "plus":
+					sendPlusBally(bot, update.Message.Chat.ID)
+				case "minus":
+					sendMinusBally(bot, update.Message.Chat.ID)
+				default:
+					sendStartMenu(bot, update.Message.Chat.ID)
+				}
 			default:
 				continue
 			}
@@ -97,8 +113,7 @@ func sendMessage(bot *tgbotapi.BotAPI, chatID int64, message string) {
 
 // Функция вызова стартового меню
 func sendStartMenu(bot *tgbotapi.BotAPI, chatID int64) {
-	msg := tgbotapi.NewMessage(chatID, "Привет! Я ваш новый семйный Telegram-бот для удобвтсва работы "+
-		"с заданиями и начилсения баллов. 😉. Выберите действие:")
+	msg := tgbotapi.NewMessage(chatID, "Выберите действие:")
 
 	// Создаем кастомную клавиатуру
 	msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
@@ -113,6 +128,34 @@ func sendStartMenu(bot *tgbotapi.BotAPI, chatID int64) {
 		),
 		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton(Help),
+		),
+	)
+	if _, err := bot.Send(msg); err != nil {
+		panic(err)
+	}
+}
+
+//Клавиатура начиления баллов
+
+func kbdSectionsPlus(bot *tgbotapi.BotAPI, chatID int64) {
+	msg := tgbotapi.NewMessage(chatID, "Выберите, сколько начислить баллов и за что: ")
+
+	msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Помощь по дому 🏠"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Учёба и саморазвитие 👨‍🎓"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Доброта и хорошее поведение 👍"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Физическая активность 🔋"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton(Back),
+			tgbotapi.NewKeyboardButton(BackToMainMenu),
 		),
 	)
 	if _, err := bot.Send(msg); err != nil {
